21711c365accc1d11a03c76034db9c4406fb60fe
Selahattin Serkan BAYAR <84895485+cadaix@users.noreply.github.com>
2022-11-07 11:03:53 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Kosullu-Ifadeler-1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("%d", 3 < 4); // 3 küçüktür 4 doğru ise true yanlış ise false döner. Doğru oldugu için true.
+getchar();  
+return 0;
+}
+
+// fonksiyonun çıktısı 1 dir. Yani true
+// True ysa 1 false ise 0 döner.
